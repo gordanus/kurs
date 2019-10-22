2855a53549686764ff9e1613e47f07c3d11d8eb9
Gordan Blažić <gblazic@smart.rs>
2019-10-22 10:32:31 +0200

    some styling improvement

--- script.js
+++ script.js
@@ -1,63 +1,104 @@
-function getPassCSS(){
+function getPassCSS() {
     var modal = document.getElementById("modalBodyCSS");
     var input = document.getElementById("passwordCSS").value;
     var btn = document.getElementById("modalBtnCSS")
     var paragraph = document.createElement("p");
-    paragraph.setAttribute("class","warning");
+    paragraph.setAttribute("class", "warning");
 
-    if(input===""){
+    if (input === "") {
         paragraph.innerHTML = "Polje ne može ostati prazno!";
         modal.append(paragraph);
-    } else if (input.toLowerCase().trim()==="cssrules"){
-        btn.setAttribute("data-dismiss","modal");
-        window.open("./CSS-sajt-resenje.zip","_self");
+    } else if (input.toLowerCase().trim() === "cssrules") {
+        btn.setAttribute("data-dismiss", "modal");
+        window.open("./CSS-sajt-resenje.zip", "_self");
     } else {
         paragraph.innerHTML = "Lozinka nije ispravna! Pokušajte ponovo.";
         modal.append(paragraph);
 
-    }  
+    }
 }
 
-function getPassJS(){
+function getPassJS() {
     var modal = document.getElementById("modalBodyJS");
     var input = document.getElementById("passwordJS").value;
     var btn = document.getElementById("modalBtnJS")
     var paragraph = document.createElement("p");
-    paragraph.setAttribute("class","warning");
+    paragraph.setAttribute("class", "warning");
 
-    if(input===""){
+    if (input === "") {
         paragraph.innerHTML = "Polje ne može ostati prazno!";
         modal.append(paragraph);
-    } else if (input.toLowerCase().trim()==="jsawesome"){
-        btn.setAttribute("data-dismiss","modal");
-        window.open("./JavaScript-resenje.zip","_self");
+    } else if (input.toLowerCase().trim() === "jsawesome") {
+        btn.setAttribute("data-dismiss", "modal");
+        window.open("./JavaScript-resenje.zip", "_self");
     } else {
         paragraph.innerHTML = "Lozinka nije ispravna! Pokušajte ponovo.";
         modal.append(paragraph);
 
-    }  
+    }
 }
 
-function getPassJQ(){
+function getPassJQ() {
     var modal = document.getElementById("modalBodyJQ");
     var input = document.getElementById("passwordJQ").value;
     var btn = document.getElementById("modalBtnJQ")
     var paragraph = document.createElement("p");
-    paragraph.setAttribute("class","warning");
+    paragraph.setAttribute("class", "warning");
 
-    if(input===""){
+    if (input === "") {
         paragraph.innerHTML = "Polje ne može ostati prazno!";
         modal.append(paragraph);
-    } else if (input.toLowerCase().trim()==="jqueryninja"){
-        btn.setAttribute("data-dismiss","modal");
-        window.open("./jQuery-resenje.zip","_self");
+    } else if (input.toLowerCase().trim() === "jqueryninja") {
+        btn.setAttribute("data-dismiss", "modal");
+        window.open("./jQuery-resenje.zip", "_self");
     } else {
         paragraph.innerHTML = "Lozinka nije ispravna! Pokušajte ponovo.";
         modal.append(paragraph);
 
-    }  
+    }
 }
+// navbar color toggle
+$(window).on('load resize', function () {
+    if ($(this).width() > 600) {
+        $(document).scroll(function () {
+            var top1 = $('#html-start').offset().top;
+            var top2 = $('#css-start').offset().top - 60;
+            var top3 = $('#js-start').offset().top - 60;
+            var scrollPos = $(document).scrollTop();
+            if (scrollPos >= top1 && scrollPos < top2) {
+                $('#myNav').addClass("navbar-dark bg-dark").removeClass("navbar-light bg-light");
+                $('#NavItemOne').addClass('active');
+                $('#NavItemTwo, #NavItemThree').removeClass('active');
+            } else if (scrollPos >= top2 && scrollPos < top3) {
+                $('#myNav').addClass("navbar-light bg-light").removeClass("navbar-dark bg-dark");
+                $('#NavItemTwo').addClass('active');
+                $('#NavItemOne, #NavItemThree').removeClass('active');
+            } else if (scrollPos >= top3) {
+                $('#myNav').addClass("navbar-dark bg-dark").removeClass("navbar-light bg-light");
+                $('#NavItemThree').addClass('active');
+                $('#NavItemOne, #NavItemTwo').removeClass('active');
 
+            } else {
+                $('#NavItemOne, #NavItemTwo, #NavItemThree').removeClass('active');
+            }
+        });
+    } else {
+        $(document).scroll(function () {
+            var top1 = $('#html-start').offset().top;
+            var top2 = $('#css-start').offset().top - 60;
+            var top3 = $('#js-start').offset().top - 60;
+            var scrollPos = $(document).scrollTop();
+            if (scrollPos >= top1 && scrollPos < top2) {
+                $('#myNav').addClass("navbar-dark bg-dark").removeClass("navbar-light bg-light");
+            } else if (scrollPos >= top2 && scrollPos < top3) {
+                $('#myNav').addClass("navbar-light bg-light").removeClass("navbar-dark bg-dark");
+            } else if (scrollPos >= top3) {
+                $('#myNav').addClass("navbar-dark bg-dark").removeClass("navbar-light bg-light");
+            }
+        }); 
+    }
+});
+// navbar color toggle end
 
 // function passwordJS() {
 //     var password = prompt("Unesite lozinku da biste preuzeli fajl:");
